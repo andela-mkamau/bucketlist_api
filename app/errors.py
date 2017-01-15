df5bb50dd134c537ddbc64d575fc22265cb213c1
Michael Kamau <michael.kamau@andela.com>
2017-01-15 15:23:08 +0300

    Add custom error handlers

---
+++ app/errors.py
@@ -0,0 +1,15 @@
+# Adopted from the works of:
+# https://github.com/miguelgrinberg/api-pycon2014
+
+from flask import jsonify
+
+
+class ValidationError(ValueError):
+    pass
+
+
+def bad_request(message):
+    response = jsonify({'status': 400, 'error': 'bad request',
+                        'message': message})
+    response.status_code = 400
+    return response
