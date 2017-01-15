72f232b64bca8033188cd730ccdc947931d06db9
Michael Kamau <michael.kamau@andela.com>
2017-01-15 12:27:18 +0300

    Add test User verifies password

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,22 +8,30 @@ class UserModelTestCase(BaseTestCase):
     """
     Tests the functionality of User model
     """
+    def setUp(self):
+        super().setUp()
+        self.mike = User(username='mike', password='secret')
 
     def test_reading_password_property_raises_exception(self):
         """
         Reading the password property should raise an AttributeError exception
         because it is a write-only property
         """
-        mike = User(username='mike', password='secret')
         with self.assertRaises(AttributeError):
-            mike_password = mike.password
+            mike_password = self.mike.password
 
     def test_generates_password_hash(self):
         """
         Given a password, the User model should create a hashed password
         """
-        alla = User(password='my password')
-        self.assertIsNotNone(alla.password_hash)
+        self.assertIsNotNone(self.mike.password_hash)
+
+    def test_verifies_password(self):
+        """
+        A User should be able to verify their own password
+        """
+        self.assertTrue(self.mike.verify_password('secret'))
+        self.assertFalse(self.mike.verify_password('newpassword'))
 
 
 if __name__ == '__main__':
