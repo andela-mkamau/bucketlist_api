fe8c16b75feb4bf6a94e982ef071e7901b4f5f8c
Michael Kamau <michael.kamau@andela.com>
2017-01-20 00:14:01 +0300

    Add test log in user with password and username

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -2,6 +2,8 @@ import unittest
 
 from tests.basetest import BaseTestCase
 from tests.testclient import TestClient
+from app import db
+from app.models import User
 
 
 class UserAuthenticationTestCase(BaseTestCase):
@@ -12,6 +14,10 @@ class UserAuthenticationTestCase(BaseTestCase):
     def setUp(self):
         super().setUp()
         self.client = TestClient(self.app)
+        user = User(username='mike',
+                    password='mike')
+        db.session.add(user)
+        db.session.commit()
 
     def test_registers_user(self):
         """
@@ -21,6 +27,16 @@ class UserAuthenticationTestCase(BaseTestCase):
             'username': 'mary', 'password': 'secret'})
         self.assertTrue(response.status_code == 201)
         self.assertTrue(json['username'] == 'mary')
+
+    def test_logs_in_user_with_password(self):
+        """
+        Should be able to log in a  User
+        """
+        response, json = self.client.post('/api/auth/login',
+                                         data={"username": "mike",
+                                               "password": "mike"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json['message'], 'user authenticated')
 
 
 if __name__ == '__main__':
