fc7d9ee209fc7cca23c9ff0bbac002eb8ceb16cc
Michael Kamau <michael.kamau@andela.com>
2017-01-23 05:30:32 +0300

    Add test verifies token

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,17 +1,20 @@
-from tests.basetest import BaseTestCase
-from app.models import User
-from app import db
-
 import unittest
+
+from app import db
+from app.models import User
+from tests.basetest import BaseTestCase
 
 
 class UserModelTestCase(BaseTestCase):
     """
     Tests the functionality of User model
     """
+
     def setUp(self):
         super().setUp()
         self.mike = User(username='mike', password='secret')
+        db.session.add(self.mike)
+        db.session.commit()
 
     def test_password_is_write_only(self):
         """
@@ -35,13 +38,14 @@ class UserModelTestCase(BaseTestCase):
         self.assertFalse(self.mike.verify_password('newpassword'))
 
     def test_generate_token(self):
-        db.session.add(self.mike)
-        db.session.commit()
-
         token = self.mike.generate_auth_token()
         self.assertIsNotNone(token)
+
+    def test_verifies_token(self):
+        token = self.mike.generate_auth_token()
+        id = self.mike.verify_auth_token(token).id
+        self.assertEqual(id, 1)
 
 
 if __name__ == '__main__':
     unittest.main()
-
