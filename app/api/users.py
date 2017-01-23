d15e632835c682abc83a003875467764cae445af
Michael Kamau <michael.kamau@andela.com>
2017-01-23 04:52:59 +0300

    Add functionality to log in user

--- app/api/users.py
+++ app/api/users.py
@@ -1,10 +1,10 @@
 # Users -- as an API resource
-from flask import request, jsonify, abort
+from flask import request
 
-from app.api import api
 from app import db
-from app.models import User
+from app.api import api
 from app.decorators import json
+from app.models import User
 
 
 @api.route('/auth/register', methods=['POST'])
@@ -17,3 +17,17 @@ def register_user():
     db.session.add(user)
     db.session.commit()
     return user, 201
+
+@api.route('/auth/login', methods=['POST'])
+@json
+def login_user():
+    """
+    Logs in a User using a token or username-password combination. The user is
+    given an authentication token valid for 60 minutes
+
+    :return: JSON Response object with status code 200 on success, else error
+    """
+    user = User().from_login_json(request.json)
+    token = user.generate_auth_token()
+    return {'token': token.decode('ascii'),
+            'message':'user authenticated'}, 200
