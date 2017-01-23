895fbb6610ee73e6656aae78c0a09672364abc97
Michael Kamau <michael.kamau@andela.com>
2017-01-23 05:16:49 +0300

    Add callback for token verification via HTTPAuth

--- app/auth.py
+++ app/auth.py
@@ -1,24 +1,22 @@
 from flask import g
-from flask_httpauth import HTTPBasicAuth
+from flask_httpauth import HTTPTokenAuth
 
 from app.errors import unauthorized
 from app.models import User
 
-auth = HTTPBasicAuth()
+auth = HTTPTokenAuth('Bearer')
 
 
-@auth.verify_password
-def verify_password(username, password):
+@auth.verify_token
+def verify_token(token):
     """
-    Callback used by HTTPAuth to check for a valid username-password
-    combination.
+    Callback used by HTTPAuth to check for a valid token
 
-    :param username: username of the `User`
-    :param password: password of the `User`
+    :param token: token provided to the `User`
     :return: `True` if credentials are correct; else `False`
     """
-    g.user = User.query.filter_by(username=username).first()
-    return g.user is not None and g.user.verify_password(password)
+    g.user = User.verify_auth_token(token)
+    return g.user is not None
 
 
 @auth.error_handler
