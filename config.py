09e7204717fbdaca7e7e58d2f4cafef05a840e8f
Michael Kamau <michael.kamau@andela.com>
2017-01-26 18:48:59 +0300

    Update configuration to fetch database URIs from environment

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
@@ -10,16 +12,20 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user@localhost/bucketlist_dev'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI_DEV') or \
+                              'postgresql://localhost/bucketlist_dev'
 
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user@localhost/bucketlist_test'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://user@localhost/bucketlist_test'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI_TEST') or \
+                              'postgresql://localhost/bucketlist_test'
     SECRET_KEY = 'test secret key'
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user@localhost/bucketlist_prod'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI_PROD') or \
+                              'postgresql://localhost/bucketlist_prod'
 
 
 config = {
